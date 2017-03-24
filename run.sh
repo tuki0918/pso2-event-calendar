217ab27ef73b54b040eeae29de611aa58f90fa4c
tuki0918 <tuki0918@gmail.com>
2017-03-24 23:35:31 +0900

    新しいdockerコマンドへの移行

--- run.sh
+++ run.sh
@@ -13,35 +13,35 @@ EOF
 case "$1" in
     # 任意のコマンド
     console)
-        /usr/local/bin/docker run --rm -it \
+        /usr/local/bin/docker container run --rm -it \
             -v ${PWD}:${WORK_DIR} \
             php:7.1-apache \
             php src/console.php $2
         ;;
     # デプロイ
     deploy)
-        /usr/local/bin/docker run --rm -it \
+        /usr/local/bin/docker container run --rm -it \
             -v ${PWD}:${WORK_DIR} \
             php:7.1-apache \
             php src/console.php calendar:fetch --deploy
         ;;
     # コーディング規約
     styling)
-        /usr/local/bin/docker run --rm -it \
+        /usr/local/bin/docker container run --rm -it \
             -v ${PWD}:${WORK_DIR} \
             php:7.1-apache \
             vendor/bin/php-cs-fixer fix -v
         ;;
     # テスト
     test)
-        /usr/local/bin/docker run --rm -it \
+        /usr/local/bin/docker container run --rm -it \
             -v ${PWD}:${WORK_DIR} \
             php:7.1-apache \
             vendor/bin/phpunit
         ;;
     # 設定
     init)
-        /usr/local/bin/docker run --rm \
+        /usr/local/bin/docker container run --rm \
             -v ${PWD}:/app \
             composer/composer:1.1-alpine install
         ;;
